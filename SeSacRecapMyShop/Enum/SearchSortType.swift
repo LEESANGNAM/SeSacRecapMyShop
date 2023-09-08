65e5628a6aed048d1b5e8bc027db55e64cdcf8f6
LEESANGNAM <fgh0116@naver.com>
2023-09-09 02:44:46 +0900

    [ Feat ] Add SearchSortType

---
+++ SeSacRecapMyShop/Enum/SearchSortType.swift
@@ -0,0 +1,40 @@
+//
+//  SearchSortType.swift
+//  SeSacRecapMyShop
+//
+//  Created by 이상남 on 2023/09/09.
+//
+
+import Foundation
+
+enum SearchSortType{
+    case sim
+    case date
+    case asc
+    case dsc
+    
+    var titleString: String {
+        switch self {
+        case .sim:
+            return "정확도"
+        case .date:
+            return "날짜순"
+        case .asc:
+            return "오름차순"
+        case .dsc:
+            return "내림차순"
+        }
+    }
+    var requstParam: String {
+        switch self {
+        case .sim:
+            return "sim"
+        case .date:
+            return "date"
+        case .asc:
+            return "asc"
+        case .dsc:
+            return "dsc"
+        }
+    }
+}

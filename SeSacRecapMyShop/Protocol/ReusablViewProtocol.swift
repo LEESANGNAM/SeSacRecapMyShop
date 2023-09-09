c76fea3f93dd78106d2a72f556abfe541ce7d617
LEESANGNAM <fgh0116@naver.com>
2023-09-09 15:21:14 +0900

    [Feat] Add ReusableViewProtocol

---
+++ SeSacRecapMyShop/Protocol/ReusablViewProtocol.swift
@@ -0,0 +1,23 @@
+//
+//  ReusablViewProtocol.swift
+//  SeSacRecapMyShop
+//
+//  Created by 이상남 on 2023/09/09.
+//
+
+import UIKit
+
+protocol ReusablViewProtocol {
+    static var identifier: String { get }
+}
+
+extension UIViewController: ReusablViewProtocol {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
+extension UICollectionViewCell: ReusablViewProtocol {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}

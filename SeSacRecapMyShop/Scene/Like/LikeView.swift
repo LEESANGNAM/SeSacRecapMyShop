d9d5f10f40514acdbc72c8c091d9ef0ed5fbbf05
LEESANGNAM <fgh0116@naver.com>
2023-09-10 04:12:47 +0900

    [Feat] Add LikeView,controller

---
+++ SeSacRecapMyShop/Scene/Like/LikeView.swift
@@ -0,0 +1,64 @@
+//
+//  LikeView.swift
+//  SeSacRecapMyShop
+//
+//  Created by 이상남 on 2023/09/10.
+//
+import UIKit
+
+class LikeView: BaseView {
+    
+    let searchSortButtonlist = SearchSortType.allCases
+    var sortButtons: [UIButton] = []
+    
+    let searchBar = {
+        let view = UISearchBar()
+        view.placeholder = "검색어를 입력해주세요"
+        view.backgroundImage = UIImage()
+        view.showsCancelButton = true
+        if let cancelButton = view.value(forKey: "cancelButton") as? UIButton {
+            cancelButton.setTitleColor(.label, for: .normal)
+            cancelButton.setTitle("취소", for: .normal)
+        }
+        return view
+    }()
+    lazy var collectionView = {
+        let view = UICollectionView(frame: .zero, collectionViewLayout: setCollectionViewLayout())
+        view.register(ProductInfoColletionViewCell.self, forCellWithReuseIdentifier: ProductInfoColletionViewCell.identifier)
+        view.collectionViewLayout = setCollectionViewLayout()
+        return view
+    }()
+    
+    func setCollectionViewLayout() -> UICollectionViewFlowLayout{
+        let layout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 10
+        // 전체 너비 가져와서 빼기
+        let width = UIScreen.main.bounds.width - (spacing * 3)
+        let itemSize = width / 2
+        layout.itemSize = CGSize(width: itemSize, height: itemSize * 1.5)
+        //컬렉션뷰 inset
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: 0, right: spacing)
+        // 최소 간격
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        return layout
+    }
+    override func setUpView() {
+        super.setUpView()
+        addSubview(searchBar)
+        addSubview(collectionView)
+    }
+    
+    override func setConstraints() {
+        
+        searchBar.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(self.safeAreaLayoutGuide).inset(5)
+            make.top.equalTo(self.safeAreaLayoutGuide)
+        }
+        collectionView.snp.makeConstraints { make in
+            make.horizontalEdges.bottom.equalTo(self.safeAreaLayoutGuide)
+            make.top.equalTo(searchBar.snp.bottom).offset(10)
+        }
+        
+    }
+}
--- SeSacRecapMyShop/Scene/Like/LikeViewController.swift
+++ SeSacRecapMyShop/Scene/Like/LikeViewController.swift
@@ -5,9 +5,14 @@
 //  Created by 이상남 on 2023/09/09.
 //
 
-import Foundation
+import UIKit
 
 class LikeViewController: BaseViewController {
+    let mainView = LikeView()
+    override func loadView() {
+        self.view = mainView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "좋아요 목록"
@@ -16,6 +21,23 @@ class LikeViewController: BaseViewController {
         super.setUpView()
     }
     override func setConstraints() {
-        
+    }
+    
+    override func setDelegate() {
+        mainView.collectionView.dataSource = self
+        mainView.collectionView.delegate = self
+    }
+}
+extension LikeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProductInfoColletionViewCell.identifier, for: indexPath) as! ProductInfoColletionViewCell
+        cell.backgroundColor = .blue
+//        let item = productList[indexPath.row]
+//        cell.setUpCellUI(item: item)
+        return cell
     }
 }

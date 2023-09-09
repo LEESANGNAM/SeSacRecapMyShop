aded7685fb46d3fba2ef4ec394b1744bc707d333
LEESANGNAM <fgh0116@naver.com>
2023-09-09 11:12:17 +0900

    [ Feat ] Add SearchButtons

--- SeSacRecapMyShop/Scene/Search/SearchView.swift
+++ SeSacRecapMyShop/Scene/Search/SearchView.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class SearchView: BaseView {
+    
+    let searchSortButtonlist = SearchSortType.allCases
+    var sortButtons: [UIButton] = []
+    
     let searchBar = {
         let view = UISearchBar()
         view.placeholder = "검색어를 입력해주세요"
@@ -20,11 +24,27 @@ class SearchView: BaseView {
         button.setTitleColor( .label , for: .normal)
         return button
     }()
-    
+    lazy var stackView = {
+        let view = UIStackView(arrangedSubviews: sortButtons)
+        view.axis = .horizontal
+        view.spacing = 5
+        view.alignment = .leading
+        view.distribution = .fillProportionally
+        return view
+    }()
+    func createSortButton(){
+        for sortType in searchSortButtonlist {
+              let button = SearchSortButton()
+            button.setTitle(sortType.titleString, for: .normal)
+            sortButtons.append(button)
+        }
+    }
     override func setUpView() {
         super.setUpView()
         addSubview(searchBar)
         addSubview(cancleButton)
+        createSortButton()
+        addSubview(stackView)
     }
     
     override func setConstraints() {
@@ -38,6 +58,11 @@ class SearchView: BaseView {
             make.leading.equalTo(searchBar.snp.trailing).offset(10)
             make.height.equalTo(searchBar.snp.height)
             make.top.equalTo(self.safeAreaLayoutGuide)
+        }
+        stackView.snp.makeConstraints { make in
+            make.leading.equalTo(self.safeAreaLayoutGuide).offset(10)
+            make.trailing.lessThanOrEqualTo(self.safeAreaLayoutGuide).offset(-20)
+            make.top.equalTo(searchBar.snp.bottom).offset(10)
         }
         
     }
--- SeSacRecapMyShop/View/SearchSortButton.swift
+++ SeSacRecapMyShop/View/SearchSortButton.swift
@@ -15,6 +15,10 @@ class SearchSortButton: UIButton {
         backgroundColor = .systemBackground
         layer.borderWidth = 1
         layer.borderColor = UIColor.systemGray4.cgColor
+        layer.cornerRadius = 10
+        setTitleColor(.black, for: .normal)
+        titleLabel?.font = .systemFont(ofSize: 14)
+        contentEdgeInsets = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")

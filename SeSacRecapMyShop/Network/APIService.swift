8941d14819898f4a76316b4c8e360f1d6f9a0582
LEESANGNAM <fgh0116@naver.com>
2023-09-09 20:05:37 +0900

    [Feat] Add APIService API호출 및 데이터 확인

---
+++ SeSacRecapMyShop/Network/APIService.swift
@@ -0,0 +1,44 @@
+//
+//  APIService.swift
+//  SeSacRecapMyShop
+//
+//  Created by 이상남 on 2023/09/09.
+//
+
+import Foundation
+import Alamofire
+
+class APIService {
+    static let shared = APIService()
+    private init() { }
+    
+    func callRequest<T: Codable>(type: SearchSortType,query: String,page: Int, completionHandler: @escaping (T) -> ()) {
+//    https://openapi.naver.com/v1/search/shop.json?query=캠핑카&display=30&start=1&sort=sim
+        let url = APIService.baseURL
+//        "query=\(query)&display=30&start=\(page)&sort=\(type.requstParam)"
+            let header: HTTPHeaders = [
+                "X-Naver-Client-Id": APIKey.ClientID,
+                "X-Naver-Client-Secret":APIKey.ClientSecret
+            ]
+            let parameters: Parameters = [
+                "query": query, //검색어
+                "display": 30, //요청 갯수
+                "start": page, // 페이지
+                "sort": type.requstParam //정렬 방법 
+            ]
+            
+            AF.request(url, method: .get, parameters: parameters, headers: header).validate()
+                .responseDecodable(of: T.self) { response in
+                    switch response.result {
+                    case .success(let value):
+                        completionHandler(value)
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
+        }
+}
+
+extension APIService {
+    static let baseURL = "https://openapi.naver.com/v1/search/shop.json?"
+}
--- SeSacRecapMyShop/Scene/Search/SearchViewController.swift
+++ SeSacRecapMyShop/Scene/Search/SearchViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class SearchViewcontroller: BaseViewController {
     let mainView = SearchView()
-    
+    var proTitle: String?
     override func loadView() {
         self.view = mainView
     }
@@ -18,6 +18,11 @@ class SearchViewcontroller: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "상품 검색"
+        APIService.shared.callRequest(type: .sim, query: "캠핑카", page: 1) { (data: ShopInfo) in
+            print(data.items)
+            self.proTitle = data.items[0].title
+            self.mainView.collectionView.reloadData()
+        }
     }
     override func setUpView() {
         super.setUpView()
@@ -40,6 +45,9 @@ extension SearchViewcontroller: UICollectionViewDelegate, UICollectionViewDataSo
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProductInfoColletionViewCell.identifier, for: indexPath) as! ProductInfoColletionViewCell
         cell.backgroundColor = .systemGray
+        if let proTitle {
+            cell.titleNameLabel.text = proTitle
+        }
         return cell
     }
     

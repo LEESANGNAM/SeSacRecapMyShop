6d4e5072b52759aa5157ec1aaf0b11fe5892e82d
LEESANGNAM <fgh0116@naver.com>
2023-09-11 19:59:44 +0900

    [Fix] 탭바컨트롤러  변수명 변경

--- SeSacRecapMyShop/Scene/TabBar/TabBarController.swift
+++ SeSacRecapMyShop/Scene/TabBar/TabBarController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class TabBarController: UITabBarController {
-    let movieTrendWeekViewController = UINavigationController(rootViewController: SearchViewcontroller())
-    let multipleViewcontroller = UINavigationController(rootViewController: LikeViewController())
+    let searchViewcontroller = UINavigationController(rootViewController: SearchViewcontroller())
+    let likeViewController = UINavigationController(rootViewController: LikeViewController())
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.viewControllers = [movieTrendWeekViewController,multipleViewcontroller]
+        self.viewControllers = [searchViewcontroller,likeViewController]
         setUpTabbar()
     }
     func setUpTabbar(){
@@ -21,10 +21,10 @@ class TabBarController: UITabBarController {
         self.tabBar.unselectedItemTintColor = .systemGray
         self.tabBar.backgroundColor = .systemBackground
         
-        movieTrendWeekViewController.tabBarItem.title = "검색"
-        movieTrendWeekViewController.tabBarItem.image = UIImage(systemName: "magnifyingglass")
-        multipleViewcontroller.tabBarItem.title = "좋아요목록"
-        multipleViewcontroller.tabBarItem.image = UIImage(systemName: "heart")
+        searchViewcontroller.tabBarItem.title = "검색"
+        searchViewcontroller.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        likeViewController.tabBarItem.title = "좋아요목록"
+        likeViewController.tabBarItem.image = UIImage(systemName: "heart")
     }
     
 }
